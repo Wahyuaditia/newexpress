d6cdbaae32ed021a239dd8f5b6198f06165348a9
Wahyuaditia <wahyuaditdiya_2327240152@mhs.mdp.ac.id>
2025-10-03 15:12:04 +0700

    p

---
+++ app_server/controllers/maincontroller.js
@@ -0,0 +1,14 @@
+const index = (req,res) =>{
+    res.render('index', {title: 'express',layout:'main'
+    });
+    const about= (req,res) =>{
+        res.render('about',{title: "About us", layout: "main"});
+    }
+
+
+    const contact = (req,res) => {
+        res.render('contact', {title:"contact"})
+    }
+
+    module.exports = {index}
+}
---
+++ app_server/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+// inport maincontroller
+const maincontroller = require('../controllers/maincontroller')
+
+// GET home page
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express'});
+  res.send('halo')
+});
+
+// GET about page
+router.get('/about', maincontroller.about);
+
+// GET contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact',{title: "Contact us", layout: "main"});
+});
+
+
+//GET prodi page
+router.get('/prodi', function(req, res, next) {
+  const data = [
+        {kode: 24,nama: "Sistem Informasi", singkatan: "SI", fakultas:"Fakultas Ilmu Komputer dan Rekayasa"},
+        {kode: 25,nama: "Informatika", singkatan: "IF", fakultas:"Fakultas Ilmu Komputer dan Rekayasa"},
+        {kode: 11,nama: "Manajemen Informatika", singkatan: "MI", fakultas:"Fakultas Ilmu Komputer dan Rekayasa"},
+        {kode: 27,nama: "Teknik Elektro", singkatan: "TE", fakultas:"Fakultas Ilmu Komputer dan Rekayasa"},
+        {kode: 20,nama: "Akutansi", singkatan: "AK", fakultas:"Fakultas Ekonomi dan Bisnis"},
+        {kode: 21,nama: "Manajemen", singkatan: "MJ", fakultas:"Fakultas Ekonomi dan Bisnis"},
+    ]
+    res.render('prodi',{data, title: "Program Studi",layout: "main"});
+} )
+
+module.exports = router;
